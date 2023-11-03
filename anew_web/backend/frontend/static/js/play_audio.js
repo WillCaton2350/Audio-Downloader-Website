fc879393973abe977c1a845192cb8fa4e0053ae2
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2023-11-03 04:50:05 -0400

    Add files via upload

---
+++ anew_web/backend/frontend/static/js/play_audio.js
@@ -0,0 +1,9 @@
+function playAudio() {
+    const audioElement = document.getElementById('myAudio'); 
+    audioElement.play(); 
+}
+
+function pauseAudio() {
+    const audioElement = document.getElementById('myAudio'); 
+    audioElement.pause(); 
+}
